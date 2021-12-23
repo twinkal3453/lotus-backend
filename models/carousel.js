da17e27d7491daa7fd33c144f922e19e38c529bf
Twinkal Kumar Raj <twinkalkrraj@gmail.com>
2021-12-23 13:57:27 +0530

    bug fixes

--- models/carousel.js
+++ models/carousel.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const carouselSchema = new mongoose.Schema({
-  photo: {
-    type: String,
+const carouselSchema = new mongoose.Schema(
+  {
+    photo: {
+      type: String,
+    },
+    name: {
+      type: String,
+      trim: true,
+      maxlength: 32,
+    },
   },
-  name: {
-    type: String,
-    trim: true,
-    maxlength: 32,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Carousel", carouselSchema);
