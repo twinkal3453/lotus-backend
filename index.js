825e2922ad05d070414fe07e9bb2c83e6a50f819
Twinkal Raj <twinkalr@systemintegration.in>
2022-07-04 23:05:29 +0530

    Swagger implementation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-import express from "express";
+import express, { application } from "express";
 import cors from "cors";
 import bodyparser from "body-parser";
 import cookieParser from "cookie-parser";
@@ -19,10 +19,34 @@ import carouselRoutes from "./routes/carousel.js";
 import ratingRoutes from "./routes/ratings.js";
 import orderRoutes from "./routes/order.js";
 import stripeRoutes from "./routes/stripePayment.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+// swagger options object
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
 
 // App Config
 const app = express();
 const connection_url = process.env.DB_URL;
+
+app.use("api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // Middleware
 app.use(express.json());
